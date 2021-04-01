9aa831b02640b972ada923ec0741f3bc13125ce5
rashidpathiyil <rashidpathiyil@gmail.com>
2021-04-01 14:41:07 +0530

    Admin

---
+++ pages/admin.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex md:h-screen text-center content-center">
+    <div class="bg-red-300 mx-auto my-auto">
+      <a href="https://wizz-api.herokuapp.com/"
+        ><button class="p-3 bg-primary text-neutral">
+          Access to Administrator
+        </button></a
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+</style>
