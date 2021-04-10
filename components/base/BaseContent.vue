1e972d73a02a60189f9b368e8836e2d681ba5875
rashidpathiyil <rashidpathiyil@gmail.com>
2021-04-10 14:39:24 +0530

    Css Update

--- components/base/BaseContent.vue
+++ components/base/BaseContent.vue
@@ -1,16 +1,9 @@
 <template>
-  <div>
-    <div
-      class="prose md:prose-xl my-12 content mx-auto text-gray-600 markdown"
-      v-if="data"
-      v-html="$md.render(data)"
-    ></div>
-    <div class="markdown text-3xl font-bold">
-      <h1>dfdfd</h1>
-      <h2>dfdfd</h2>
-      <h3>dfdfd</h3>
-    </div>
-  </div>
+  <div
+    class="prose md:prose-xl my-12 content mx-auto text-gray-600 markdown"
+    v-if="data"
+    v-html="$md.render(data)"
+  ></div>
 </template>
 
 <script>
