7986070a4f1ecb5dc1ef0eec9194c8284e92085f
rashidpathiyil <rashidpathiyil@gmail.com>
2021-08-10 15:17:54 +0530

    Fix Video URL Issue

--- store/navigation.js
+++ store/navigation.js
@@ -90,7 +90,7 @@ export const useNavigationStore = defineStore({
                         },
                         {
                             title: "Videos",
-                            slug: "/videos",
+                            slug: "#",
                         }
                     ]
                 },
